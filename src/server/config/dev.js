9a858e70139898d262f281fce723da90592d02d3
Kamalimari K <Kamalimari.K@mrcooper.com>
2021-08-27 11:43:46 +0530

    changed namings

--- src/server/config/dev.js
+++ src/server/config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
-  port: PROCESS.ENV.port,
-  databaseName: PROCESS.ENV.databaseName,
-  userName: PROCESS.ENV.userName,
-  password: PROCESS.ENV.password,
-  host: PROCESS.ENV.host
+  port: process.env.PORT,
+  databaseName: process.env.DATABASE_NAME,
+  userName: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  host: process.env.HOST
 }
